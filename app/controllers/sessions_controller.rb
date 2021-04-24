74ff22e7930f74db43862ce13da791d93cbea9e4
Jesse <nguyenjessev@gmail.com>
2021-04-24 11:52:17 -0400

    Create skeleton SessionsController

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,17 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by_name(params[:name])
+    if user
+      session[:user_id] = user.id
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to root_path
   end
 end
