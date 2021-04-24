110f18ee88c1eaa0561f7285708b2d9201b5c5f5
Jesse <nguyenjessev@gmail.com>
2021-04-24 11:32:00 -0400

    Generate Sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root 'users#index'
 end
