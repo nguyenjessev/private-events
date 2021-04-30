2a4e60636a20ceae22d97bc62fe1caefdd5f451d
Jesse <nguyenjessev@gmail.com>
2021-04-29 21:29:46 -0400

    generate EventsController

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'events/index'
+  get 'events/new'
+  get 'events/create'
+  get 'events/show'
   resources :users
   resources :sessions
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
