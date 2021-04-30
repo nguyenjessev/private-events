50bcb39ccc2e114a28e6e1bfc80e165617f3a727
Jesse <nguyenjessev@gmail.com>
2021-04-29 22:13:22 -0400

    Allow users to create Events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,13 +1,34 @@
 class EventsController < ApplicationController
-  def index
-  end
+  before_action :require_login, except: %i[index show]
+
+  def index; end
 
   def new
+    @event = Event.new
   end
 
   def create
+    @event = User.find(session[:user_id]).created_events.build(event_params)
+
+    if @event.save
+      redirect_to @event
+    else
+      render :new
+    end
   end
 
-  def show
+  def show; end
+
+  private
+
+  def require_login
+    return if User.exists?(session[:user_id])
+
+    flash[:error] = 'You must be logged in to create an event'
+    redirect_to new_session_path
+  end
+
+  def event_params
+    params.require(:event).permit(:title, :description, :date)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-  get 'events/index'
-  get 'events/new'
-  get 'events/create'
-  get 'events/show'
   resources :users
   resources :sessions
+  resources :events
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root 'users#index'
