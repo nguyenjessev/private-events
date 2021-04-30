7894906b7fbe1f9ec9ccc8139acf2cce5ca379c8
Jesse <nguyenjessev@gmail.com>
2021-04-29 21:24:02 -0400

    Add creator_id to events

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   validates :name, presence: true, uniqueness: true
 
-  has_many :created_events, foreign_key: 'creator_id', class_name: 'Event'
+  has_many :created_events, foreign_key: 'creator_id', class_name: 'Event', dependent: :destroy
 end
