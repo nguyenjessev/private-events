ee09b36fbb829d478e9bb3c5dca888ebd171b83d
Jesse <nguyenjessev@gmail.com>
2021-05-08 11:50:42 -0400

    Migrate database

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_30_011833) do
+ActiveRecord::Schema.define(version: 2021_05_08_151430) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "event_attendances", force: :cascade do |t|
+    t.bigint "event_attendee_id"
+    t.bigint "attended_event_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["attended_event_id"], name: "index_event_attendances_on_attended_event_id"
+    t.index ["event_attendee_id"], name: "index_event_attendances_on_event_attendee_id"
+  end
 
   create_table "events", force: :cascade do |t|
     t.string "title"
@@ -31,4 +40,6 @@ ActiveRecord::Schema.define(version: 2021_04_30_011833) do
     t.index ["name"], name: "index_users_on_name", unique: true
   end
 
+  add_foreign_key "event_attendances", "events", column: "attended_event_id"
+  add_foreign_key "event_attendances", "users", column: "event_attendee_id"
 end
