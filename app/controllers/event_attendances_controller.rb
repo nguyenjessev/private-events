63c7b9e571ac059a5963e21822722e7c87e5e45d
Jesse <nguyenjessev@gmail.com>
2021-05-14 20:25:48 -0400

    Allow Users to add/remove their own attendances.

--- app/controllers/event_attendances_controller.rb
+++ app/controllers/event_attendances_controller.rb
@@ -7,5 +7,15 @@ class EventAttendancesController < ApplicationController
     attendance.event_attendee_id = params[:attendee_id]
 
     attendance.save
+    redirect_to event_path(params[:event_id]), notice: 'You have successfully registered your attendance to this event.'
+  end
+
+  def destroy
+    EventAttendance.where(attended_event_id: params[:event_id], event_attendee_id: params[:attendee_id]).destroy_all
+
+    respond_to do |format|
+      format.html { redirect_to event_path(params[:event_id]), notice: 'You have successfully canceled your attendance to this event.' }
+      format.json { head :no_content }
+    end
   end
 end
