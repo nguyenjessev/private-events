1a13cb56c0ccbb9f3e4ae61ee612ee36f2cb1ad0
Jesse <nguyenjessev@gmail.com>
2021-05-15 15:54:46 -0400

    Separate upcoming and past events

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,7 @@ class Event < ApplicationRecord
 
   has_many :event_attendances, foreign_key: :attended_event_id, dependent: :destroy
   has_many :attendees, through: :event_attendances, source: :event_attendee
+
+  scope :upcoming, -> { where('date >= ?', Date.today) }
+  scope :past, -> { where('date < ?', Date.today) }
 end
